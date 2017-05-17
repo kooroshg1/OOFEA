b75bf63071636a1f06fab081c71c443b97ebfc61
Koorosh Gobal <gobalk@gobalmac.local>
2017-05-17 16:36:03 -0400

    Imrpoved vulan mesh reader

--- Python/src/mesh/mesh.py
+++ Python/src/mesh/mesh.py
@@ -1,58 +1,96 @@
 import numpy as np
 
 class NODE():
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, info=None):
+        self.id = int(info[1])
+        self.x = float(info[2])
+        self.y = float(info[3])
+        self.z = float(info[4])
         self.dof = None
+        self.get_dof()
 
     def get_dof(self):
         self.dof = [0, 1, 2, 3, 4, 5]
-        self.dof = [i + self.node_number * 6 for i in self.dof]
+        self.dof = [i + self.id * 6 for i in self.dof]
 
 
-class ELEMENT(NODE):
-    def __init__(self):
+class ELEMENT():
+    def __init__(self, info=None):
+        self.id = int(info[1])
+        self.property = int(info[2])
         self.node = []
-        self.node_number = -1
+        for node in info[3:]:
+            self.node.append(int(node))
 
-    def add_node(self, x, y, z):
-        self.node_number += 1
-        self.node.append(NODE(x, y, z))
+class PROPERTY():
+    def __init__(self, info=None):
+        self.id = int(info[1])
+        self.type = info[2]
+        self.prop = [float(prop) for prop in info[3:]]
+
+class MATERIAL():
+    def __init__(self, info=None):
+        self.id = int(info[1])
+        self.prop = [float(prop) for prop in info[2:]]
+
 
 
 class MESH(ELEMENT):
     def __init__(self):
         print 'Initializing mesh ..'
         self.element = []
-        self.element_number = -1
-        self.number_of_nodes = 0
+        self.node = []
+        self.property = dict()
+        self.material = dict()
+        self.mesh_file_path = None
+        self.mesh_options = {'NODE': self.add_node,
+                             'ELEMENT': self.add_element,
+                             'PROPERTY': self.add_property,
+                             'MATERIAL': self.add_material}
 
-    def add_element(self):
-        self.element_number += 1
-        self.element.append(ELEMENT())
+    def add_node(self, line):
+        self.node.append(NODE(info=line))
 
-    def get_number_of_nodes(self):
-        for element in self.element:
-            self.number_of_nodes += (element.node_number + 1)
+    def add_element(self, line):
+        self.element.append(ELEMENT(info=line))
+
+    def add_property(self, line):
+        self.property[int(line[1])] = PROPERTY(info=line)
+
+    def add_material(self, line):
+        self.material[int(line[1])] = MATERIAL(info=line)
+
+    def read_mesh(self):
+        with open(self.mesh_file_path) as inputfile:
+            for line in inputfile:
+                if line[0] == '#':
+                    continue
+                line = line[:-1].split(',')
+                try:
+                    self.mesh_options[line[0]](line)
+                except AttributeError:
+                    continue
+                except KeyError:
+                    continue
+                except NameError:
+                    continue
+                except TypeError:
+                    continue
 
 mesh = MESH()
-mesh.add_element()
-mesh.add_element()
+mesh.mesh_file_path = 'sample.inp'
+mesh.read_mesh()
 
-mesh.element[0].add_node(0, 0, 0)
-mesh.element[0].add_node(1, 0, 0)
-mesh.element[0].add_node(1, 1, 0)
-mesh.element[0].add_node(0, 1, 0)
+# for node in mesh.node:
+#     print node.id, node.x, node.y, node.z, node.dof
 
-mesh.element[1].add_node(1, 0, 0)
-mesh.element[1].add_node(2, 0, 0)
+# for element in mesh.element:
+#     print element.id, element.property, element.node
 
-mesh.get_number_of_nodes()
-print mesh.number_of_nodes
+# for property in mesh.property:
+#     print property.id, property.type, property.prop
 
-# for el in mesh.element:
-#     print el.node_number
-#     for node in el.node:
-#         print node.x, node.y, node.z
+
+# for node in mesh.node:
+#     print node.id, node.x, node.y, node.z, node.dof
+# node = globals()['NODE'](1)
